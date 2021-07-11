959e5fde735b7a84d21da9150a90d4b08341b27c
bnn848 <77334083+bnn848@users.noreply.github.com>
2021-07-11 11:08:06 +0900

    FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const App = () => {
-  return <div>React Starter Kit in TypeScript</div>;
+// 型定義はinterfaceで定義してPropsで用いる
+interface AppProps {
+  message?: string; // <-- オプショナルな型定義にしないとindex.tsの方でエラーになる
+}
+
+// コンポーネントに対して型定義する
+const App: React.FunctionComponent<AppProps> = ({ message }) => { // 型引数を受け取ることができる
+  return <div>{message}</div>;
 };
+
+// .defaultProps
+App.defaultProps = { // コンポーネントのPropsに初期値を設定することができる
+  message: 'hello Default!',
+  // message: 123, <-- interfaceの定義と異なる値だとエラー
+  // description: 'This is App Component.' <-- interfaceでdescriptionを定義してないとエラー
+}
 
 export default App;
